3fda5947df901eb0554b749b1549bc1f2c14ccec
Norton <78592780+SystemAdmi@users.noreply.github.com>
2023-09-03 01:02:30 +0300

    Update stylesClient.js

--- stylesClient.js
+++ stylesClient.js
@@ -1,155 +1,55 @@
-const style = document.createElement('style');
+const style = document.createElement('style')
 style.textContent = `
- .chaotic-orbit {
- --uib-size: 25px;
- --uib-speed: 1.5s;
- --uib-color: black;
- position: relative;
- display: flex;
- align-items: center;
- justify-content: center;
- height: var(--uib-size);
- width: var(--uib-size);
- animation: rotate936 calc(var(--uib-speed) * 1.667) infinite linear;
+h1 {
+    color: white;
+    margin-left: 50px;
+}
+.bottomContent {
+    background-color: rgba(0, 0, 0, 0.7);
+    border-radius: 10px;
+    color: white;
+    padding: 10px;
+    width: 100%;
+    height: 100px;
+    position: fixed;
+    top: 95%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 }
 
-.chaotic-orbit::before,
-.chaotic-orbit::after {
- content: '';
- position: absolute;
- height: 60%;
- width: 60%;
- border-radius: 50%;
- background-color: var(--uib-color);
- will-change: transform;
- flex-shrink: 0;
+.play_button {
+    display: inline-block;
+    border-radius: 10px;
+    border: none;
+    background: #1875FF;
+    color: white;
+    font-family: inherit;
+    text-align: center;
+    font-size: 30px;
+    box-shadow: 0px 20px 56px -11px #1875FF;
+    width: 10em;
+    padding: 1em;
+    transition: all 0.4s;
+    cursor: pointer;
+    position: fixed;
+    top: 70%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 400px;
+    height: 100px;
+}
+   
+.play_button span {
+    cursor: pointer;
+    display: inline-block;
+    position: relative;
+    transition: 0.4s;
 }
 
-.chaotic-orbit::before {
- animation: orbit var(--uib-speed) linear infinite;
+.play_button:hover {
+    box-shadow: 0px 30px 56px -5px #1875FF;
+    width: 410px;
+    height: 110px;
 }
-
-.chaotic-orbit::after {
- animation: orbit var(--uib-speed) linear calc(var(--uib-speed) / -2)
-    infinite;
-}
-
-@keyframes rotate936 {
- 0% {
-  transform: rotate(0deg);
- }
-
- 100% {
-  transform: rotate(360deg);
- }
-}
-
-@keyframes orbit {
- 0% {
-  transform: translate(calc(var(--uib-size) * 0.5)) scale(0.73684);
-  opacity: 0.65;
- }
-
- 5% {
-  transform: translate(calc(var(--uib-size) * 0.4)) scale(0.684208);
-  opacity: 0.58;
- }
-
- 10% {
-  transform: translate(calc(var(--uib-size) * 0.3)) scale(0.631576);
-  opacity: 0.51;
- }
-
- 15% {
-  transform: translate(calc(var(--uib-size) * 0.2)) scale(0.578944);
-  opacity: 0.44;
- }
-
- 20% {
-  transform: translate(calc(var(--uib-size) * 0.1)) scale(0.526312);
-  opacity: 0.37;
- }
-
- 25% {
-  transform: translate(0%) scale(0.47368);
-  opacity: 0.3;
- }
-
- 30% {
-  transform: translate(calc(var(--uib-size) * -0.1)) scale(0.526312);
-  opacity: 0.37;
- }
-
- 35% {
-  transform: translate(calc(var(--uib-size) * -0.2)) scale(0.578944);
-  opacity: 0.44;
- }
-
- 40% {
-  transform: translate(calc(var(--uib-size) * -0.3)) scale(0.631576);
-  opacity: 0.51;
- }
-
- 45% {
-  transform: translate(calc(var(--uib-size) * -0.4)) scale(0.684208);
-  opacity: 0.58;
- }
-
- 50% {
-  transform: translate(calc(var(--uib-size) * -0.5)) scale(0.73684);
-  opacity: 0.65;
- }
-
- 55% {
-  transform: translate(calc(var(--uib-size) * -0.4)) scale(0.789472);
-  opacity: 0.72;
- }
-
- 60% {
-  transform: translate(calc(var(--uib-size) * -0.3)) scale(0.842104);
-  opacity: 0.79;
- }
-
- 65% {
-  transform: translate(calc(var(--uib-size) * -0.2)) scale(0.894736);
-  opacity: 0.86;
- }
-
- 70% {
-  transform: translate(calc(var(--uib-size) * -0.1)) scale(0.947368);
-  opacity: 0.93;
- }
-
- 75% {
-  transform: translate(0%) scale(1);
-  opacity: 1;
- }
-
- 80% {
-  transform: translate(calc(var(--uib-size) * 0.1)) scale(0.947368);
-  opacity: 0.93;
- }
-
- 85% {
-  transform: translate(calc(var(--uib-size) * 0.2)) scale(0.894736);
-  opacity: 0.86;
- }
-
- 90% {
-  transform: translate(calc(var(--uib-size) * 0.3)) scale(0.842104);
-  opacity: 0.79;
- }
-
- 95% {
-  transform: translate(calc(var(--uib-size) * 0.4)) scale(0.789472);
-  opacity: 0.72;
- }
-
- 100% {
-  transform: translate(calc(var(--uib-size) * 0.5)) scale(0.73684);
-  opacity: 0.65;
- }
-}
-`;
-
-document.head.appendChild(style);
+`
+document.head.appendChild(style)
